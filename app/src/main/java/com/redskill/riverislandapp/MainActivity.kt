159b9a44819746ade37377acd525da8133962cb9
Fabio Arosio <fabio_arosio@hotmail.it>
2020-07-08 12:25:26 +0100

    Code Refactoring in MainActivity

--- app/src/main/java/com/redskill/riverislandapp/MainActivity.kt
+++ app/src/main/java/com/redskill/riverislandapp/MainActivity.kt
@@ -38,30 +38,19 @@ class MainActivity : AppCompatActivity()  {
             true
         }
 
-
-
-
-        val TAG = "MyActivity"
-        Log.d(TAG,"Inizio")
-
-
-        var list = fetchJson("https://static-r2.ristack-3.nn4maws.net/v1/plp/en_gb/2506/products.json")
-        val productList2 : ArrayList<ProductInfo> = ArrayList()
+        // Delete TAG for Debugging
+        val TAG = "myActivity"
+        // Fetching ProductList from Server
+        fetchJson("https://static-r2.ristack-3.nn4maws.net/v1/plp/en_gb/2506/products.json")
+        val allProducts : ArrayList<ProductInfo> = ArrayList()
         for (item in productList) {
-            productList2.add(item)
+            allProducts.add(item)
         }
-
-        Log.d(TAG,productList2[1].name)
-
-
-//        val gson = Gson()
-//        var itemType = object : TypeToken<List<Product>>(){}.type
-//        var productList : List<Product> = gson.fromJson(list,itemType)
-        Log.d(TAG,"Fine")
+        Log.d(TAG,allProducts[1].name)
 
     }
-    fun fetchJson(url: String) : String {
-        var myString = "Empty"
+    fun fetchJson(url: String)  {
+
         val TAG1 = "MyActivity"
         Log.d(TAG1,"Attempting to fetch Json")
 
@@ -70,17 +59,10 @@ class MainActivity : AppCompatActivity()  {
         client.newCall(request).enqueue(object: Callback {
 
             override fun onResponse(call: Call, response: Response) {
-                Log.d(TAG1,"Attempting to fetch Json 2")
                 val body = response.body?.string()
-                myString = body.toString()
-                Log.d(TAG1,"Attempting to fetch Json 3")
+
                 val jsonObject = JSONObject(body)
-
-                Log.d(TAG1,"Attempting 4")
                 var jsonArray_productList:JSONArray= jsonObject.getJSONArray("Products")
-
-                Log.d(TAG1,"Attempting to fetch Json 5")
-
                 var i = 0;
                 var size = jsonArray_productList.length()
                 for (i in 0.. size-1) {
@@ -91,26 +73,14 @@ class MainActivity : AppCompatActivity()  {
                     json_objectdetail.getInt("cost"),
                     json_objectdetail.getString("category"))
                     productList.add(productInfo)
-
-
                 }
-
-//                val gson = Gson()
-//                val list2 = object : TypeToken<List<Product>>(){}.type
-//                val realList = gson.fromJson<Product>(jsonArray_productList.toString(),list2)
-                Log.d(TAG1,"Attempting to fetch Json 6")
-
-
             }
 
             override fun onFailure(call: Call, e: IOException) {
                 print("Failed to execute request")
             }
         })
-        Log.d(TAG1,"Attempting to fetch Json 5")
-        Thread.sleep(3000)
-
-        return myString
+        Thread.sleep(2000)
     }
     private fun makeCurrentFragment(fragment:Fragment) =
         supportFragmentManager.beginTransaction().apply {
@@ -120,37 +90,3 @@ class MainActivity : AppCompatActivity()  {
 
 }
 
-/*
-fun fetchSeatById(seatIDList: List<Int>) : MutableList<String> {
-    val seatList = mutableListOf<String>()
-    var counter = 0
-    for (e in seatIDList) {
-        var id = seatIDList[counter].toString()
-        val url  = "http://10.0.2.2:8080/seat/$id"
-        counter++
-        val request = Request.Builder().url(url).build()
-        var myJsonSeat = "Seat"
-        val response = client.newCall(request).enqueue(object : Callback {
-
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body?.string()
-                seatList.add(body.toString())
-
-            }
-
-            override fun onFailure(call: Call, e: IOException) {
-                print("Error")
-            }
-        })
-
-
-    }
-    Thread.sleep(1000)
-    return seatList
-}
-        val customer = Customer(customer_id,customer_name,customer_surname,customer_email)
-        val seats = fetchSeatById(selected_seats).toString()
-
-        // TO DO: Fix line below
-        val itemTypeSeat = object : TypeToken<ArrayList<Seat>>() {}.type
-        val userSeats  = gson.fromJson<ArrayList<Seat>>(seats,itemTypeSeat)*/
